5b492372b1faf625b04349c6b305641790bc8a03
wontae <83wontae@gmail.com>
2022-06-27 08:37:20 +0900

    aaaa

---
+++ Ex2/Sum.h
@@ -0,0 +1,6 @@
+#pragma once
+
+//함수 이름 : NewSum
+//매개 변수(파라메타) : int a, int b
+//반환 값 : int
+//a와 b를 합한 결과를 반환하시오
---
+++ Ex2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+//함수 이름 : Sum
+//매개 변수(파라메타) : int a, int b
+//반환 값 : int
+//a와 b를 합한 결과를 반환하시오
+
+
+int main()
+{
+	// 2개의 int형 숫자을 받아서 두수의 합을 출력하시오
+}
