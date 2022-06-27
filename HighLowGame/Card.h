04e4f469d7eda7b49a5f8ef94a266ed84156af5d
wontae <83wontae@gmail.com>
2022-06-27 15:55:54 +0900

    aaa

--- HighLowGame/Card.h
+++ HighLowGame/Card.h
@@ -1,6 +1,10 @@
 #pragma once
+#include <iostream>
+#include <string>
 
 #define Max 52
+
+using namespace std;
 
 enum CardMark
 {
@@ -14,5 +18,28 @@ struct stCard
 {
 	CardMark mark;
 	int number;
+
+	// 함수 이름 : printCard
+	// 파라메타 : 없음
+	// 반환값 : 없음
+	// Mark = Spade Number = 7 <--- 이렇게 화면에 출력하는 함수를 만드시오
+	void printCard()
+	{
+		cout << "Mark = " << GetStrMark() << " Number = " << number << endl;
+	}
+
+	string GetStrMark()
+	{
+		switch (mark)
+		{
+		case Clover:	return "Clover";	break;
+		case Heart:	return "Heart";	break;
+		case Dia:		return "Dia";		break;
+		case Spade:	return "Spade";	break;
+		default:		return "";			break;
+		}
+	}
+
+	
 };
 
--- HighLowGame/main.cpp
+++ HighLowGame/main.cpp
@@ -18,14 +18,72 @@
 
 using namespace std;
 
+// 함수이름 : CompareCard
+// 파라메타 : const stCard& CardA, const stCard& CardB
+// 반환값 : bool
+// 함수기능 : CardA와 CardB를 비교하여 CardA가 크다면 true 작으면 false 반환
+bool CompareCard(const stCard& CardA, const stCard& CardB)
+{
+	// CardA와 CardB를 비교하여 CardA가 크다면 true 작으면 false 반환
+	// 위와 같이 출력한다.
+	// (크기 비교 조건) Number : 1 ~ 13 숫자가 높을수록 높은 카드
+	// (크기 비교 조건) Number가 같다면 
+	// 스페이트 > 다이아 > 하트 > 클로버 순으로 높을수록 높은 카드
+	if (CardA.number == CardB.number) {
+		if (CardA.mark > CardB.mark) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	else if (CardA.number > CardB.number) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
 void main()
 {
-	Initialize();
-	Shuffle();
+	Initialize(); // 카드 52장의 정보를 넣어준다.
+	Shuffle(); // 카드를 섞는다.
 
+	// 0번 카드 출력
+	cards[0].printCard();
+	char chChoose = ' ';
+	cin >> chChoose;
+	// chChoose가 'H'고 (cards[1] > cards[0]) 이면 "승리" 출력 아니면 "패배" 출력
+	// chChoose가 'L'고 (cards[1] < cards[0]) 이면 "승리" 출력 아니면 "패배" 출력
+	switch (chChoose)
+	{
+	case 'H' :
+		if (CompareCard(cards[0], cards[1]))	{
+			cout << "패배" << endl;
+		}
+		else {
+			cout << "승리" << endl;
+		}
+		break;
+	case 'L' :
+		if (CompareCard(cards[0], cards[1])) {
+			cout << "승리" << endl;
+		}
+		else {
+			cout << "패배" << endl;
+		}
+		break;
+	}
+
+
+
+	cards[1].printCard();
 	// 카드 출력
+	/*
 	for (int i = 0; i < Max; ++i)
 	{
 		cout << "Mark = " << cards[i].mark << " Number = " << cards[i].number << endl;
 	}
+	*/
 }
